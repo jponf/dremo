a3768764016ee2d9bcac0c74aa9df0f3f39cb574
Josep Pon Farreny <jponfarreny@gmail.com>
2013-11-21 00:35:59 +0100

    Added more system information to sysinfo

--- src/sysinfo.py
+++ src/sysinfo.py
@@ -4,6 +4,27 @@
 import psutil          # https://code.google.com/p/psutil/wiki/Documentation
 import platform
 
+# Determines the operating system in module load time
+PYTHON_SYSTEM = platform.system()
+OS_IS_OSX = PYTHON_SYSTEM.lower() == 'darwin' and platform.mac_ver()[0]
+OS_IS_WIN = PYTHON_SYSTEM.lower() == 'windows'
+OS_IS_LINUX = PYTHON_SYSTEM.lower() == 'linux'
+
+OS_NAME = ""
+OS_VERSION= ""
+
+if OS_IS_LINUX:
+	OS_NAME, OS_VERSION, _ = platform.linux_distribution()
+
+elif OS_IS_OSX:
+	OS_NAME = "Mac OS X"
+	OS_VERSION, _, _ = platform.mac_ver()
+
+elif OS_IS_WIN:
+	OS_NAME = "Windows"
+	OS_VERSION, _, _, _ = platform.win32_ver()
+
+# Encapsualte system info data and methods to retrive it
 class SysInfo:
 
 	def __init__(self):
@@ -22,9 +43,9 @@ class SysInfo:
 
 		self._cpu_usage_percent = [0] * psutil.NUM_CPUS
 
-		self._cur_tasks = set()
-		self._started_tasks = set()
-		self._finished_tasks = set()
+		self._cur_procs = frozenset()
+		self._started_procs = frozenset()
+		self._finished_procs = frozenset()
 
 	def updateMemory(self):
 		"""updateMemory() -> void
@@ -53,19 +74,98 @@ class SysInfo:
 
 		# TODO: cpuload
 
-	def updateRunningTasks(self):
-		"""updateCPU() -> void
+	def updateProcesses(self):
+		"""updateProcesses() -> void
 
 		Updates the set of current running and tasks that have finished and have
 		started since the last call to this function.
 
 		"""
-		cur_tasks = set(psutil.process_iter())
+		cur_procs = frozenset( [ (p.pid, p.name) \
+								for p in psutil.process_iter()] )
 
-		self._started_tasks = cur_tasks - self._cur_tasks
-		self._finished_tasks = self._cur_tasks - cur_tasks
+		self._started_procs = cur_procs - self._cur_procs
+		self._finished_procs = self._cur_procs - cur_procs
 
-		self._cur_tasks = cur_tasks
+		self._cur_procs = cur_procs
+
+	def getOperatingSystemName(self):
+		"""getOperatingSystemName() -> str
+
+		Returns the name of the operating system 
+
+		"""
+		if OS_NAME: return OS_NAME
+
+		raise Exception("Unknown operating system")
+
+	def getOperatingSystemVersion(self):
+		"""getOperatingSystemVersion() -> str
+
+		Returns the version of the operating system
+
+		"""
+		if OS_VERSION: return OS_VERSION
+
+		return OS_VERSION
+
+
+	def getCPUPercentage(self):
+		"""getCPUPercentage() -> [float]
+
+		Returns a list or tuple filled with the cpu usage (%) at the last call 
+		to updateCPU()
+
+		"""
+		return self._cpu_usage_percent
+
+	def getVirtualMemoryInfo(self):
+		"""getVirtualMemoryInfo() -> (total, used, free, avaliable)
+
+		Returns a tuple with the total memory in the system, the used memory,
+		the free memory and the avaliable memory.
+
+		Note: In some systems the avaliable memory is not equals to free memory
+
+		"""
+		return (self._total_mem, self._used_mem, self._avaliable_mem, \
+			self._free_mem)
+
+	def getSwapMemoryInfo(self):
+		"""getSwapMemoryInfo() -> (total, used, swap)
+
+		Returns a tuple with the total swap memory, the used swap memory and 
+		the free swap memory.
+
+		"""
+		return (self._total_swap, self._used_swap, self._free_swap)
+
+	def getRunningProcesses(self):
+		"""getRunningProcesses() -> set<(pid, name)>
+
+		Returns a set filled with processes that were running at the last call
+		to updateProcesses().
+
+		"""
+		return self._cur_procs
+
+	def getStartedProcesses(self):
+		"""getStartedProcesses() -> set<(pid, name)>
+
+		Returns a set filled with the processes whose execution have started
+		between two calls to updateProcesses()
+
+		"""
+		return self._started_procs
+
+	def getFinishedProcesses(self):
+		"""getFinishedProcesses() -> set<(pid, name)>
+
+		Returns a set filled with the processes whose execution have finished
+		between two calls to updateProcesses()
+
+		"""
+		return self._finished_procs
 
 #
 #
@@ -76,7 +176,7 @@ if __name__ == '__main__':
 	def updateAllSysInfo(sinfo):
 		sinfo.updateMemory()
 		sinfo.updateCPU()
-		sinfo.updateRunningTasks()
+		sinfo.updateProcesses()
 
 	sinfo = SysInfo()
 	updateAllSysInfo(sinfo)
@@ -85,10 +185,26 @@ if __name__ == '__main__':
 
 	updateAllSysInfo(sinfo)
 
-	print "Finished tasks:"
-	for t in sinfo._finished_tasks:
+	print "Operating System:", sinfo.getOperatingSystemName(), \
+								sinfo.getOperatingSystemVersion()
+	print
+	print "Virtual Memory (total, used, free, avaliable)"
+	print sinfo.getVirtualMemoryInfo()
+	print
+	print "Swap Memory (total, used, free)"
+	print sinfo.getSwapMemoryInfo()
+	print
+	print "CPU use percentage"
+	print sinfo.getCPUPercentage()
+	print
+	print "Finished processes:"
+	for t in sinfo.getStartedProcesses():
 		print "\t", t
-
-	print 'Started tasks:'
-	for t in sinfo._started_tasks:
+	print
+	print 'Started processes:'
+	for t in sinfo.getFinishedProcesses():
+		print "\t", t
+	print
+	print 'Running processes:'
+	for t in sinfo.getRunningProcesses():
 		print "\t", t
