82fefe2d4723e6db64a5cca1fa1779629d42d272
Josep Pon Farreny <jponfarreny@gmail.com>
2013-11-29 12:38:25 +0100

    Updated clean script

--- clean.sh
+++ clean.sh
@@ -1,38 +1,52 @@
 #!/usr/bin/env sh
 #
-# Remove all the .pyc and .pyo files from the root directory of the project
+# Remove all the .pyc and .pyo files from the specified root directory 
 # and all its subdirectories
 #
 
+cleanDirectory()
+{
+    
+    echo "+++ Cleaning Directory $1"
+    removeCompiledPythonFiles $1
+
+    # Look for subdirectories
+    for entry in $1/*; do
+        
+        if [[ -d $entry && $entry != .. ]]; then
+            cleanDirectory $entry
+        fi
+    done    
+    echo "--- Cleaned Directory $1"
+}
+
+removeCompiledPythonFiles()
+{
+    rm -vf $1/*.pyc
+    rm -vf $1/*.pyo
+}
+
+
+#
+# Main
 if [[ $# -lt 1 || $# -gt 1 ]]; then
-	echo ""
-	echo "Usage: $0 root_directory"
-	echo ""
-	echo "This script remove all .pyc and .pyo files from root_directory"
-	echo "and all the directories under <root_folder>"
+    echo ""
+    echo "Usage: $0 root_directory"
+    echo ""
+    echo "This script remove all .pyc and .pyo files from root_directory"
+    echo "and all the directories under <root_folder>"
 else
 
-	if [ -d $1 ]; then
-		echo ""
-		echo "### Removing *.pyc and *.pyo files ###"
-		echo ""
-		rm -vf *.pyc
-		rm -vf *.pyo
-
-		for entry in $1/*; do
-			# Remove pyc and pyo files from the directory
-			if [[ -d $entry && $entry != .. ]]; then
-				echo "+++ Entering subidrectory $entry"
-				cd $entry
-				rm -vf *.pyc
-				rm -vf *.pyo
-				cd ..
-				echo "--- Leaving subdirectory $entry"
-			fi
-		done
-
-		echo "--- Finished"
-	else
-		echo "'$1' is not a directory"
-	fi
+    if [ -d $1 ]; then
+        echo ""
+        echo "### Cleaning... ###"
+        echo ""   
+        cleanDirectory $1
+        echo ""
+        echo "### Finished ###"
+        echo ""
+    else
+        echo "'$1' is not a directory"
+    fi
 fi
+
